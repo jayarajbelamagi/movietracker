e19fd647b2f7804df586c0ac938acfb1e28d88f3
Jayaraj U B <ubelamagi@gmail.com>
2025-08-25 10:40:21 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -10,7 +10,7 @@ function Register({ setIsLoggedIn }) {
     setError("");
 
     // 1️⃣ Register user
-    const res = await fetch("http://localhost:5000/api/users/register", {
+    const res = await fetch("https://movietrackerbackend.onrender.com/api/users/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
